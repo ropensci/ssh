9191a05fe941fd6fb84dfee16bef2eae3c2470e9
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-01 22:37:15 +0100

    Test checks

--- R/tunnel.R
+++ R/tunnel.R
@@ -3,7 +3,11 @@
 #' Create an SSH tunnel
 #'
 #' @export
+#' @rdname ssh
 #' @useDynLib tunnel C_blocking_tunnel
+#' @param host a hostname
+#' @param port a number
+#' @param user the username to authenticate with
 blocking_tunnel <- function(host = "dev.opencpu.org", port = 22, user = me()) {
   stopifnot(is.character(host))
   stopifnot(is.numeric(port))
@@ -12,6 +16,7 @@ blocking_tunnel <- function(host = "dev.opencpu.org", port = 22, user = me()) {
 }
 
 #' @export
+#' @rdname ssh
 me <- function(){
   Sys.info()[["user"]]
 }
