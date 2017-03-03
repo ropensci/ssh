ff1df5bf37138fc7148b5251369ad1abe853d521
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-03 14:56:46 +0100

    Switch to recv() for socket reading

--- src/tunnel.c
+++ src/tunnel.c
@@ -99,7 +99,7 @@ void tunnel_port(ssh_session ssh, int port, const char * outhost, int outport){
     while(!pending_interrupt()){
       //assume connfd is non-blocking
       int avail = 1;
-      while((avail = read(connfd, buf, sizeof(buf))) > 0)
+      while((avail = recv(connfd, buf, sizeof(buf), 0)) > 0)
         ssh_channel_write(tunnel, buf, avail);
       bail_for(avail == -1, "read()");
       while((avail = ssh_channel_read_timeout(tunnel, buf, sizeof(buf), FALSE, waitms)) > 0)
