4fc10ba75d02726a4197a0094fd2e51f02895b8c
Jeroen <jeroenooms@gmail.com>
2018-01-30 14:37:48 +0100

    Support filenname output

--- R/command.R
+++ R/command.R
@@ -2,12 +2,26 @@
 #' @rdname ssh
 #' @useDynLib ssh C_ssh_exec
 #' @param command The command to execute (e.g. `ls ~/ -al | grep -i reports`).
-#' @param std_out callback function or connection to handle stdout stream from host
-#' @param std_err callback function or connection to handle stderr stream from host
+#' @param std_out callback function, filename, or connection object to handle stdout stream
+#' @param std_err callback function, filename, or connection object to handle stderr stream
 ssh_exec <- function(session = ssh_connect(), command = "whoami", std_out = stdout(), std_err = stderr()) {
   assert_session(session)
   stopifnot(is.character(command))
   command <- paste(command, collapse = "\n")
+
+  # Convert TRUE or filepath into connection objects
+  std_out <- if(isTRUE(std_out) || identical(std_out, "")){
+    stdout()
+  } else if(is.character(std_out)){
+    file(normalizePath(std_out, mustWork = FALSE))
+  } else std_out
+
+  std_err <- if(isTRUE(std_err) || identical(std_err, "")){
+    stderr()
+  } else if(is.character(std_err)){
+    std_err <- file(normalizePath(std_err, mustWork = FALSE))
+  } else std_err
+
   outfun <- if(inherits(std_out, "connection")){
     if(!isOpen(std_out)){
       open(std_out, "wb")
@@ -43,7 +57,7 @@ ssh_exec <- function(session = ssh_connect(), command = "whoami", std_out = stdo
     }
   }
   status <- .Call(C_ssh_exec, session, command, outfun, errfun)
-  if(is.na(out))
+  if(is.na(status))
     return(invisible())
   status
 }
