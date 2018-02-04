fdc4e928386d91ad4e4fd71bd278f011b235bcc1
Jeroen <jeroenooms@gmail.com>
2018-02-05 00:57:38 +0100

    Refactor reading file

--- src/scp.c
+++ src/scp.c
@@ -1,27 +1,3 @@
-#include "myssh.h"
-#include <sys/stat.h>
-#include <libgen.h>
-
-
-/* Retrieve file from remote host */
-SEXP C_scp_read_file(SEXP ptr, SEXP path){
-  ssh_session ssh = ssh_ptr_get(ptr);
-  ssh_scp scp = ssh_scp_new(ssh, SSH_SCP_READ, CHAR(STRING_ELT(path, 0)));
-  bail_if(ssh_scp_init(scp), "ssh_scp_init", ssh);
-  bail_if(ssh_scp_pull_request(scp) != SSH_SCP_REQUEST_NEWFILE, "SSH_SCP_REQUEST_NEWFILE", ssh);
-
-  /* get file info */
-  R_xlen_t size = ssh_scp_request_get_size64(scp);
-  SEXP out = Rf_allocVector(RAWSXP, size);
-  bail_if(ssh_scp_accept_request(scp), "ssh_scp_accept_request", ssh);
-  if(ssh_scp_read(scp, RAW(out), size) != size)
-    Rf_error("Read bytes did not match filesize");
-  bail_if(ssh_scp_pull_request(scp) != SSH_SCP_REQUEST_EOF, "SSH_SCP_REQUEST_EOF", ssh);
-  ssh_scp_close(scp);
-  ssh_scp_free(scp);
-  return out;
-}
-
 /* Convert modes to integer:
  * > as.integer(as.octmode(as.character('755')))
  * 493
@@ -29,6 +5,8 @@ SEXP C_scp_read_file(SEXP ptr, SEXP path){
  * 420
  */
 
+#include "myssh.h"
+#include <libgen.h>
 
 static void enter_directory(ssh_scp scp, char * path, ssh_session ssh){
   char subdir[4000];
@@ -36,23 +14,6 @@ static void enter_directory(ssh_scp scp, char * path, ssh_session ssh){
   if(strcmp(path, basename(path)))
     enter_directory(scp, dirname(path), ssh);
   bail_if(ssh_scp_push_directory(scp, subdir, 493L), "ssh_scp_push_directory", ssh);
-}
-
-SEXP C_scp_write_file(SEXP ptr, SEXP path, SEXP data){
-  ssh_session ssh = ssh_ptr_get(ptr);
-  ssh_scp scp = ssh_scp_new(ssh, SSH_SCP_WRITE | SSH_SCP_RECURSIVE, ".");
-  bail_if(ssh_scp_init(scp), "ssh_scp_init", ssh);
-  char cpath[4000];
-  char filename[4000];
-  strncpy(cpath, CHAR(STRING_ELT(path, 0)), 4000);
-  strncpy(filename, basename(cpath), 4000);
-  if(strcmp(cpath, filename))
-    enter_directory(scp, dirname(cpath), ssh);
-  bail_if(ssh_scp_push_file(scp, filename, Rf_length(data), 420L), "ssh_scp_push_file", ssh);
-  bail_if(ssh_scp_write(scp, RAW(data), Rf_length(data)), "ssh_scp_write", ssh);
-  ssh_scp_close(scp);
-  ssh_scp_free(scp);
-  return path;
 }
 
 /* Example: https://github.com/jeroen/libssh/blob/master/examples/scp_download.c
@@ -78,6 +39,37 @@ static SEXP stream_to_r(ssh_scp scp){
     size -= read_bytes;
   } while(size != 0);
   return out;
+}
+
+
+/* Retrieve file from remote host */
+SEXP C_scp_read_file(SEXP ptr, SEXP path){
+  ssh_session ssh = ssh_ptr_get(ptr);
+  ssh_scp scp = ssh_scp_new(ssh, SSH_SCP_READ, CHAR(STRING_ELT(path, 0)));
+  bail_if(ssh_scp_init(scp), "ssh_scp_init", ssh);
+  bail_if(ssh_scp_pull_request(scp) != SSH_SCP_REQUEST_NEWFILE, "SSH_SCP_REQUEST_NEWFILE", ssh);
+  SEXP out = stream_to_r(scp);
+  bail_if(ssh_scp_pull_request(scp) != SSH_SCP_REQUEST_EOF, "SSH_SCP_REQUEST_EOF", ssh);
+  ssh_scp_close(scp);
+  ssh_scp_free(scp);
+  return out;
+}
+
+SEXP C_scp_write_file(SEXP ptr, SEXP path, SEXP data){
+  ssh_session ssh = ssh_ptr_get(ptr);
+  ssh_scp scp = ssh_scp_new(ssh, SSH_SCP_WRITE | SSH_SCP_RECURSIVE, ".");
+  bail_if(ssh_scp_init(scp), "ssh_scp_init", ssh);
+  char cpath[4000];
+  char filename[4000];
+  strncpy(cpath, CHAR(STRING_ELT(path, 0)), 4000);
+  strncpy(filename, basename(cpath), 4000);
+  if(strcmp(cpath, filename))
+    enter_directory(scp, dirname(cpath), ssh);
+  bail_if(ssh_scp_push_file(scp, filename, Rf_length(data), 420L), "ssh_scp_push_file", ssh);
+  bail_if(ssh_scp_write(scp, RAW(data), Rf_length(data)), "ssh_scp_write", ssh);
+  ssh_scp_close(scp);
+  ssh_scp_free(scp);
+  return path;
 }
 
 SEXP C_scp_download_recursive(SEXP ptr, SEXP path, SEXP cb){
