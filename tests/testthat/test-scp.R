2870e9bc92d202ae888e32c5f97540af18ae5510
Jeroen <jeroenooms@gmail.com>
2018-02-05 12:32:39 +0100

    Improve unit test for scp uploading/downloading

--- tests/testthat/test-scp.R
+++ tests/testthat/test-scp.R
@@ -4,11 +4,19 @@ content <- function(x){
   unname(tools::md5sum(x))
 }
 
-test_that("uploading a directory", {
+compare_dir <- function(ssh, source_dir){
+  res <- ssh_exec_internal(ssh, paste('ls', source_dir))
+  lst <- strsplit(rawToChar(res$stdout), "\\s+")[[1]]
+  expect_equal(lst, list.files(source_dir))
+}
+
+test_that("Upload and download a directory", {
   ssh <- ssh_connect()
-  ssh_exec_internal(ssh, command = "rm -Rf ~/testdir")
+  expect_equal(ssh_exec_internal(ssh, command = "rm -Rf ~/testdir")$status, 0)
   source_dir <- 'testdir'
   scp_upload(ssh, files = source_dir, to = "~", verbose = FALSE)
+  compare_dir(ssh, source_dir)
+  compare_dir(ssh, file.path(source_dir, 'subdir', 'subsubdir'))
   scp_download(ssh, files = source_dir, to = tempdir(), verbose = FALSE)
   target_dir <- file.path(tempdir(), 'testdir')
   expect_equal(list.files(source_dir, recursive = TRUE), list.files(target_dir, recursive = TRUE))
