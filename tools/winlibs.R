8bcf97ca9b5d4daa46f64e9a99e7c2862c0fc854
Jeroen Ooms <jeroenooms@gmail.com>
2018-02-05 21:30:26 +0100

    Windows: update to libssh 0.7.5

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from rwinlib
-if(!file.exists("../windows/libssh-0.7.4/include/libssh/libssh.h")){
+if(!file.exists("../windows/libssh-0.7.5/include/libssh/libssh.h")){
   if(getRversion() < "3.3.0") stop("This package requires R 3.3 or newer")
-  download.file("https://github.com/rwinlib/libssh/archive/v0.7.4.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libssh/archive/v0.7.5.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
