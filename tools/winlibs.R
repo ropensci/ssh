bd072feb92acf90da4983b72ed7acde34f5b8652
Jeroen Ooms <jeroenooms@gmail.com>
2018-02-07 13:35:34 +0100

    Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
 # Build against static libraries from rwinlib
 if(!file.exists("../windows/libssh-0.7.5/include/libssh/libssh.h")){
-  if(getRversion() < "3.3.0") stop("This package requires R 3.3 or newer")
+  if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/libssh/archive/v0.7.5.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
