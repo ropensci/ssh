c5cdfbd3c6a8d9114bf65ba534478b271d18a97e
Jeroen <jeroenooms@gmail.com>
2018-05-27 00:09:28 +0200

    Rename vignette input

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,4 @@
 #Vignettes that depend on internet access have been precompiled:
 
 library(knitr)
-knit("vignettes/intro.Rmd.in", "vignettes/intro.Rmd")
+knit("vignettes/intro.Rmd.in.Rmd", "vignettes/intro.Rmd")
