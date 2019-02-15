f71f9354c2a6774264c4cc8fe9fb71c107f6667f
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-15 17:58:05 +0100

    Fix Windows build

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,8 +3,8 @@ if(getRversion() < "3.3.0") setInternet2()
 VERSION <- commandArgs(TRUE)
 
 # Downloads libssh2 + dependencies
-if(!file.exists(sprintf("../windows/libssh-%s/include/git2.h", VERSION))){
-  download.file(sprintf("https://github.com/libssh/libgit2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+if(!file.exists(sprintf("../windows/libssh-%s/include/libssh/libssh.h", VERSION))){
+  download.file(sprintf("https://github.com/rwinlib/libssh/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
