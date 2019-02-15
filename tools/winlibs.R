17b359a7e62874ba2354af657ea96ee809b8db6b
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-15 17:36:47 +0100

    Windows: update to libssh 0.8.6

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,10 @@
-# Build against static libraries from rwinlib
-if(!file.exists("../windows/libssh-0.8.3/include/libssh/libssh.h")){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/libssh/archive/v0.8.3.zip", "lib.zip", quiet = TRUE)
+# We need https
+if(getRversion() < "3.3.0") setInternet2()
+VERSION <- commandArgs(TRUE)
+
+# Downloads libssh2 + dependencies
+if(!file.exists(sprintf("../windows/libssh-%s/include/git2.h", VERSION))){
+  download.file(sprintf("https://github.com/libssh/libgit2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
