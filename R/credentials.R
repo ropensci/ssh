db6540cca2c258f09c448bcf4d3663272fd7bba9
Jeroen Ooms <jeroenooms@gmail.com>
2019-03-26 15:02:25 +0100

    Import credential utilities

---
+++ R/credentials.R
@@ -0,0 +1,27 @@
+#' @importFrom credentials ssh_key_info
+#' @rdname credentials
+#' @name credentials
+#' @title ssh authentication utilities
+#' @family ssh
+#' @export
+credentials::ssh_key_info
+
+#' @importFrom credentials ssh_keygen
+#' @rdname credentials
+#' @export
+credentials::ssh_keygen
+
+#' @importFrom credentials ssh_read_key
+#' @rdname credentials
+#' @export
+credentials::ssh_read_key
+
+#' @importFrom credentials ssh_home
+#' @rdname credentials
+#' @export
+credentials::ssh_home
+
+#' @importFrom credentials ssh_agent_add
+#' @rdname credentials
+#' @export
+credentials::ssh_agent_add
