86d52c9201155a9f69e82f16a7bd08cbf2875585
Jeroen Ooms <jeroenooms@gmail.com>
2019-03-26 15:05:15 +0100

    Rename stuff

--- R/credentials.R
+++ R/credentials.R
@@ -1,27 +1,27 @@
 #' @importFrom credentials ssh_key_info
-#' @rdname credentials
-#' @name credentials
-#' @title ssh authentication utilities
+#' @rdname ssh_credentials
+#' @name ssh_credentials
+#' @title Managing Your SSH Key
 #' @family ssh
 #' @export
 credentials::ssh_key_info
 
 #' @importFrom credentials ssh_keygen
-#' @rdname credentials
+#' @rdname ssh_credentials
 #' @export
 credentials::ssh_keygen
 
 #' @importFrom credentials ssh_read_key
-#' @rdname credentials
+#' @rdname ssh_credentials
 #' @export
 credentials::ssh_read_key
 
 #' @importFrom credentials ssh_home
-#' @rdname credentials
+#' @rdname ssh_credentials
 #' @export
 credentials::ssh_home
 
 #' @importFrom credentials ssh_agent_add
-#' @rdname credentials
+#' @rdname ssh_credentials
 #' @export
 credentials::ssh_agent_add
